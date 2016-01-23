99160fc7cecb6954ea6bbdbd7a9904b518cfa255
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-01-23 22:53:02 +0530

    Plots have been Generated

--- Colored Walls/Part1/src/manifold.py
+++ Colored Walls/Part1/src/manifold.py
@@ -4,7 +4,9 @@ from sklearn import manifold
 import sys
 from os import mkdir
 
+
 LOL = int(sys.argv[1])
+
 
 if LOL == -5000:
 	NumIter=500
@@ -12,6 +14,7 @@ else:
 	X_cor = float(sys.argv[1])
 	Y_cor = float(sys.argv[2])
 	NumIter = int(sys.argv[3])
+
 
 images_Data = []
 for i in range(1,NumIter):
@@ -23,8 +26,9 @@ for i in range(1,NumIter):
 	img.load()
 	data = (np.array(img)).flatten()
 	images_Data.append(data)
-
 images_Data = np.array(images_Data)
+
+
 
 try:
 	N_Neighbours = int(sys.argv[4])
@@ -32,12 +36,20 @@ except:
 	print "Number of Neighbours not specified. Assuming 10"
 	N_Neighbours = 10
 
+
+
 print "Trying to fit a 2d manifold"
-Manifold_Transform_2  = manifold.Isomap(N_Neighbours, 2).fit_transform(images_Data)
+Manifold_2  		= 	manifold.Isomap(N_Neighbours, 2)
+Data_Transform_2	=	Manifold_2.fit_transform(images_Data)
+
+
+
 print "Trying to fit a 3d manifold"
-Manifold_Transform_3  = manifold.Isomap(N_Neighbours, 3).fit_transform(images_Data)
+Manifold_3			= 	manifold.Isomap(N_Neighbours, 3).fit_transform(images_Data)
+Data_Transform_3	=   Manifold_3.fit_transform(images_Data)
 
 print "Manifolds now Created. Writing to files"
+
 
 try:
 	mkdir('Reduced_data')
@@ -45,10 +57,12 @@ try:
 except:
 	print "Everything seems OK"
 
+
 if LOL==-5000:
 	fold = 'Reduced_data/data_Rnd'
 else:
 	fold ='Reduced_data/data_'+str(X_cor)+'_'+str(Y_cor)+'_'+str(NumIter)+'_'+str(N_Neighbours)
+
 
 try:
 	mkdir(fold)
@@ -56,10 +70,12 @@ try:
 except:
 	print "Everything STILL seems OK"
 
+
 file2  = fold +'/Dimensions_2.csv'
 file3  = fold +'/Dimensions_3.csv'
 
-np.savetxt(file2,Manifold_Transform_2,delimiter=",")
-np.savetxt(file3,Manifold_Transform_3,delimiter=",")
+
+np.savetxt(file2,Data_Transform_2,delimiter=",")
+np.savetxt(file3,Data_Transform_3,delimiter=",")
 
 print "Files have been created"
